60a3874c0782cb226a40fb57e5678de76524cb8f
João <joaopedr8945@gmail.com>
2021-09-07 11:08:58 -0300

    ok

--- reactjs/site/src/pages/devschool/conteudo.js
+++ reactjs/site/src/pages/devschool/conteudo.js
@@ -5,12 +5,23 @@ export default function Conteudo() {
     
     return (
         <ContainerConteudo>
-            <div>
-                <div>oooo</div>
+            <div className="container1">
+                <div className="sloga">
+                    <img className="logo" src="/assents/images/Vector.png" alt="" />
+                    <div className="titulo"><span>Dev</span> School</div>
+                </div>
+                <div className="vazio">
+
+                </div>
+                <div className="geren">
+                    <div className="opcao1"> Gerenciamento <img className="setaprabaixo" src="/assents/images/setinhaprabaixo.png" alt="" /> </div>
+                </div>
+                <div className="alun">
+                    <div className="barra1roxo"></div>
+                    <div className="opcao2">Alunos</div>
+                </div>
             </div>
-            <div>
-                <div>--oooo</div>
-            </div>
+            <div className="container2"></div>
         </ContainerConteudo>
         
         
--- reactjs/site/src/pages/devschool/conteudostyle.js
+++ reactjs/site/src/pages/devschool/conteudostyle.js
@@ -4,8 +4,71 @@ import styled from 'styled-components'
 const ContainerConteudo = styled.div`
     display: flex;
     flex-direction: row;
-   
 
+    .container1 {
+        background-color: #2B3031;
+    }
+
+    .sloga {
+        display: flex;
+        flex-direction: row;
+        padding: 2em 2em;
+        align-items: center;
+    }
+
+    .logo {
+        
+    }
+
+    .titulo {
+        padding-left: 0.5em;
+        font-size: 1.5em;
+        color: white;
+    }
+    .titulo span {
+        color: #EA10C7;
+        margin-right: -5px;
+    }
+
+    .vazio {
+        background: rgba(38, 38, 38, 1);
+        width: auto;
+        height: 3em;
+    }
+
+    .geren {
+        display: flex;
+        align-items: center;
+        width: 17em;
+        height: 3em;
+        padding: 2em;
+        color: white;
+    }
+    .setaprabaixo{
+        padding-left: 5.7em;
+    }
+
+    .alun {
+        display: flex;
+        align-items: center;
+        
+
+    }
+
+    .opcao2 {
+        display: flex;
+        align-items: center;
+        width: 17em;
+        height: 3em;
+        padding: 2em;
+        color: rgba(26, 26, 26, 1);
+        background: rgba(255, 255, 255, 1);
+    }
+    .barra1roxo {
+        background: rgba(219, 33, 189, 1);
+        width: 0.2em;
+        height: 4em;
+    }
 `
 
 
